6549bb33fd40070875d940493c80a57abcd0f758
SamZRetzlaff <73126723+SamZRetzlaff@users.noreply.github.com>
2020-11-19 08:20:18 -0800

    React app setup and framework

---
+++ pilot/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  //CONSTRUCTOR
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+  }
+  //METHODS
+
+  //RENDER
+  render() {
+    return (
+      <div >
+    
+      </div>
+    )
+  }
+}
+
+export default App;
