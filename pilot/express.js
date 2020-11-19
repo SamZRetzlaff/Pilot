f527fb1b41534a90e5039b24a88f63003e78655f
SamZRetzlaff <73126723+SamZRetzlaff@users.noreply.github.com>
2020-11-19 13:40:54 -0800

    finished intial setup of express

--- pilot/express.js
+++ pilot/express.js
@@ -12,12 +12,44 @@ const dummyUser = JSON.parse(fs.readFileSync('./dummyUser.json', 'utf-8'))
 const dummyPilot = JSON.parse(fs.readFileSync('./dummyPilot.json', 'utf-8'))
 
 //List of all Users
-
+app.get('/Users', (req,res) =>{
+    res.send(dummyUser)
+})
 //List of all Pilots
+app.get('/Pilots', (req,res) =>{
+    res.send(dummyPilot)
+})
+// //Get a User by Username
+// app.get('/Users/:Username', (req,res) =>{
+    
+// })
+// //Get a Pilot by Pilot ID
+// app.get('/Pilots/:Pilot_ID', (req,res) =>{
+    
+// })
+// //Update a Pilots File, given a pilot ID
+// app.patch('/Pilots/:Pilot_ID', (req,res) =>{
+    
+// })
+// //Get a list of all flights
+// app.get('/Flights', (req,res) =>{
+    
+// })
+// //Get list of flights for a given pilot
+// app.get('Flights/:Pilot_ID', (req,res) =>{
+    
+// })
+// //Get a single flight, from a list of flights for a given pilot
+// app.get('Flights/:Pilot_ID/:Flight_ID', (req,res) =>{
+    
+// })
+// //delete a flight, from a list of flights, for a given pilot
+// app.delete('Flights/:Pilot_ID/:Flight_ID', (req,res) =>{
+    
+// })
+// //update a flight, from a list of flights, for a given pilot
+// app.patch('Flights/:Pilot_ID/:Flight_ID', (req,res) =>{
+    
+// })
 
-//Get a User by Username
-
-//Get a Pilot by Pilot ID
-
-
-
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
