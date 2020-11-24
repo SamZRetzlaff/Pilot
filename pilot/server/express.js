d78451b83fd0288dd1e9b2aa968a44f832119304
SamZRetzlaff <73126723+SamZRetzlaff@users.noreply.github.com>
2020-11-24 14:20:50 -0800

    non-working docker update
    messing with docker

--- pilot/server/express.js
+++ pilot/server/express.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require("body-parser")
 const app = express()
-const database = require('./queries')
+const database = require('../src/queries')
 const port = 3001
 app.use(cors())
 app.use(bodyParser.json())
